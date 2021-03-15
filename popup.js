9ff32e00429dbf5cdcf08be024251badebb35325
hiromoo <muta.hiroki408@mail.kyutech.jp>
2021-03-15 18:12:34 +0900

    add "Get syllabuses" button. (ver.1.1.0)

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-$("#btn").on("click", () => {
+$("#btn_download").on("click", () => {
     chrome.storage.local.get(data => {
         const bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
         const content = JSON.stringify(data);
@@ -6,4 +6,7 @@ $("#btn").on("click", () => {
         const url = URL.createObjectURL(blob);
         window.open(url, "_blank");
     });
+});
+$("#btn_get").on("click", () => {
+    window.open("https://edragon-syllabus.jimu.kyutech.ac.jp/guest/syllabuses/direct?year=2021", "_blank");
 });
